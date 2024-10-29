7da822eb0b8e931c8375d4aac4d0fa9b9c1df4a9
RyanBeaumont <rbeaumon@msudenver.edu>
2024-10-28 19:15:19 -0600

    fixed streaks, pie chart, and empty list notifications

--- components/OrganizerAlgorithm.js
+++ components/OrganizerAlgorithm.js
@@ -1,9 +1,8 @@
 const OrganizerAlgorithm = {
-    GetPriority: (dueDateString, userPriority, taskLength) => {
+    GetPriority: (dueDateString, isCompleted, taskLength) => {
       // Priority calculation logic
       const weightPerDay = 1; //+1 priority point per day up to a max of 5
       const weightOfDueToday = 7;
-      const weightOfUserPriority = 5; //+5 if the user marks a task High Priority
       const weightPerHour = 2; //+1 priority point per hour in the task session length
       const today = new Date();
       const dueDate = new Date(dueDateString);
@@ -12,10 +11,8 @@ const OrganizerAlgorithm = {
       score += weightOfDueToday-(weightPerDay*daysTilDue);
       const length = parseFloat(taskLength,10);
       if(!isNaN(length)){
-        score += weightPerHour*taskLength;
+        score += weightPerHour*(taskLength - (taskLength*isCompleted));
       }
-      score += weightOfUserPriority*userPriority;
-      //console.log("Priority "+score)
       if(!isNaN(score)) return score;
       else return 0;
     }
